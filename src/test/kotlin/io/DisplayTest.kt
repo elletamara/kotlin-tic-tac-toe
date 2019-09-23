118704cf890e0fd9951783bf298350b5deea67bd
Elle Hallal <ellejblogs@gmail.com>
2019-09-23 12:41:35 +0100

    Create Display class - displayBoard outputs the board

--- src/test/kotlin/io/DisplayTest.kt
+++ src/test/kotlin/io/DisplayTest.kt
@@ -1,5 +1,47 @@
-package IO
+package io
 
+import board.Grid
+import board.Square
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
-internal class DisplayTest
+internal class DisplayTest {
+
+    private fun defaultGrid(): Grid {
+        val squares = arrayListOf(
+            Square(1),
+            Square(2),
+            Square(3),
+            Square(4),
+            Square(5),
+            Square(6),
+            Square(7),
+            Square(8),
+            Square(9)
+        )
+        return Grid(squares)
+    }
+
+    @Test
+    fun `displays the board`() {
+        val expectedOutput = """
+            1 | 2 | 3
+            ---------
+            4 | 5 | 6
+            ---------
+            7 | 8 | 9
+            
+            
+        """.trimIndent()
+        val grid = defaultGrid()
+        val output = ByteArrayOutputStream()
+        val consoleIO = ConsoleIO(PrintStream(output))
+        val display = Display(consoleIO)
+
+        display.displayBoard(grid)
+
+        assertEquals(expectedOutput, output.toString())
+    }
+}
