2b6d059e682c936ba3302f701e31f830b8953d3f
Elle Hallal <ellejblogs@gmail.com>
2019-09-23 16:05:15 +0100

    Refactor tests and TicTacToe - condense generation of squares

--- src/main/kotlin/TicTacToe.kt
+++ src/main/kotlin/TicTacToe.kt
@@ -5,17 +5,11 @@ import io.ConsoleIO
 import io.Display
 
 fun main(args: Array<String>) {
-    val squares = arrayListOf(
-        Square(1),
-        Square(2),
-        Square(3),
-        Square(4),
-        Square(5),
-        Square(6),
-        Square(7),
-        Square(8),
-        Square(9)
-    )
+    val squares: ArrayList<Square> = arrayListOf()
+    val squareValues: IntArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    for(value in squareValues) {
+        squares.add(Square(value))
+    }
     val grid =  Grid(squares)
     val board = Board(grid)
     val consoleIO = ConsoleIO()
---
+++ src/test/kotlin/board/SquaresGeneratorTest.kt
@@ -0,0 +1,5 @@
+package board
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class SquaresGeneratorTest
