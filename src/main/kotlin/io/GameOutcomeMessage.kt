45ea1e5c6676bcae86affb27e3a15241ce040d6f
Elle Hallal <ellejblogs@gmail.com>
2019-09-24 19:28:33 +0100

    GameOutcomeMessage - create GameOutcomeMessage enum for messages displayed to the user

---
+++ src/main/kotlin/io/GameOutcomeMessage.kt
@@ -0,0 +1,6 @@
+package io
+
+enum class GameOutcomeMessage(val string: String) {
+    WIN("Congratulations "),
+    TIE("It's a tie!")
+}
---
+++ src/test/kotlin/game/GameControllerTest.kt
@@ -0,0 +1,5 @@
+package game
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class GameControllerTest
