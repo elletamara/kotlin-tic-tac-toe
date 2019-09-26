e8b30113c66df3a0d84c7c0c866e24900b5250f1
Elle Hallal <ellejblogs@gmail.com>
2019-09-26 17:04:23 +0100

    Player: rename parameter opponentMark to opponentsMark

--- src/main/kotlin/player/HumanPlayer.kt
+++ src/main/kotlin/player/HumanPlayer.kt
@@ -1,14 +1,13 @@
 package player
 
 import board.Board
-import io.BoardPresenter
 import io.InputValidator
 
 internal class HumanPlayer(private val mark: String, private val inputValidator: InputValidator): Player {
 
-    override fun chooseMove(board: Board, currentPlayersMark: String, opponentMark: String): Int {
+    override fun chooseMove(board: Board, currentPlayersMark: String, opponentsMark: String): Int {
 
-        return inputValidator.validateMove(board, getMark(), opponentMark)
+        return inputValidator.validateMove(board, getMark(), opponentsMark)
     }
 
     override fun getMark(): String {
