e7a066fde9afaaf36347bcb29bfa9ee108782d15
Elle Hallal <ellejblogs@gmail.com>
2019-09-26 11:22:59 +0100

    MinimaxStrategy - add bestScoreForMinimisingPlayer(), returns the best score in a hash

--- src/main/kotlin/player/moveStrategy/MinimaxStrategy.kt
+++ src/main/kotlin/player/moveStrategy/MinimaxStrategy.kt
@@ -1,10 +1,18 @@
 package player.moveStrategy
 
 import board.Board
+import kotlin.collections.HashMap
+
 
 internal class MinimaxStrategy: MoveStrategy {
 
     override fun getMove(board: Board, currentPlayersMark: String, opponentsMark: String): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    fun bestScoreForMaximisingPlayer(scores: HashMap<Int, Int>): Int {
+        val max = scores.maxBy { it.value }
+        return max!!.value
+    }
 }
+
--- src/test/kotlin/player/moveStrategy/MinimaxStrategyTest.kt
+++ src/test/kotlin/player/moveStrategy/MinimaxStrategyTest.kt
@@ -6,4 +6,24 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class MinimaxStrategyTest {
 
+    @Test
+    fun `returns the best scores from the scores `() {
+        val strategy = MinimaxStrategy()
+        val movesAndScores: HashMap<Int, Int> = hashMapOf(
+            1 to -7,
+            2 to -8,
+            3 to 5,
+            4 to 3,
+            5 to 9,
+            6 to 8,
+            7 to -1,
+            8 to 0,
+            9 to -3
+        )
+        val bestScore = strategy.bestScoreForMaximisingPlayer(movesAndScores)
+
+        assertEquals(9, bestScore)
+    }
 }
+
+
